9c4ec8dcb7702624690d668ca9a0d3222aed66a5
HunterVT <77024344+HunterVT@users.noreply.github.com>
2024-11-09 01:22:51 -1000

    test chart

---
+++ app/src/pages/PowerConsumption.js
@@ -0,0 +1,19 @@
+<<<<<<< Updated upstream
+=======
+import NavbarComponent from '../components/Navbar'
+import FooterComponent from '../components/Footer'
+
+import BarChart from '../components/BarChart'
+
+const PowerConsumption = () => {
+    return (
+        <div>
+            <NavbarComponent />
+            <BarChart/>
+            <FooterComponent />
+        </div>
+    );
+};
+
+export default PowerConsumption;
+>>>>>>> Stashed changes
