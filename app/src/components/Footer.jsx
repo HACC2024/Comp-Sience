3e0095899c198dc8c18e69bd79ea9e89b2949c45
elijah-saloma <esaloma@hawaii.edu>
2024-11-11 01:47:13 -1000

    finished now

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -3,8 +3,19 @@ import { MDBFooter } from 'mdb-react-ui-kit';
 
 function FooterComponent() {
   return (
-    <MDBFooter bgColor='dark' className='text-center text-lg-left' style={{ bottom: 0, width: '100%', backgroundColor: 'black', color: 'white' }}>
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)' }}>
+    <MDBFooter 
+      bgColor='dark' 
+      className='text-center text-lg-left' 
+      style={{ 
+        bottom: 0, 
+        width: '100%', 
+        backgroundColor: 'black', 
+        color: 'white', 
+        fontFamily: "'Patrick Hand', cursive", 
+        fontSize: '1.5rem' // Set the desired font size here
+      }}
+    >
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)', fontSize: '1.5rem' }}>
         Made By: {' Adam Winfield-Smith, Phat Ca, Elijah Saloma, Jared Lo, Victor Pagan, Hunter Von Tungeln'}
       </div>
     </MDBFooter>
